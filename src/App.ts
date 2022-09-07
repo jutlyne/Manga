23c6bd9f096ac1f1ceb500903016df394d54d986
kyvc-flydino <kyvc@flydinotech.com>
2022-09-07 14:03:23 +0700

    add download image crawl data

--- src/App.ts
+++ src/App.ts
@@ -1,20 +1,6 @@
 import 'dotenv/config'
 import * as express from 'express';
-import cheerio from 'cheerio';
-import axios from 'axios';
-
-interface Manga {
-	name: string;
-	updatedAt: string;
-	status?: string;
-	author?: string;
-	categories?: object;
-	views?: string;
-	image?: string;
-	content?: string;
-	totalVotes?: string;
-	rate?: string;
-}
+import Crawl from './crawl/index.controller';
 
 class App {
   public express;
@@ -26,63 +12,18 @@ class App {
     this.mountRoutes()
   }
 
-  private mountRoutes (): void {
+  private mountRoutes(): void {
     const router = express.Router();
-    const arr = [];
-    router.get('/', async (req, res) => {
-      for await (let url of this.crawlListMangas(this.defaultPageUrl)) {
-        let a = await this.scrapeManga(url);
-        arr.push(a);
+    const dataRes = [];
+    router.get('/', async (req: express.Request, res: express.Response) => {
+      for await (let url of Crawl.crawlListMangas(this.defaultPageUrl)) {
+        let manga = await Crawl.scrapeManga(url);
+        dataRes.push(manga);
       }
-      res.json(arr);
-    })
+      res.json(dataRes);
+    });
+
     this.express.use('/', router)
-  }
-
-  async *crawlListMangas(url: string) {
-    const response = await axios.get(url);
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const links = $('.ModuleContent .items .item').get();
-
-    for (let link of links) {
-      const href = $(link).find('figure.clearfix .image a').attr('href');
-      const mangaPageUrl = new URL(href, url).toString();
-
-      if (mangaPageUrl) {
-        yield mangaPageUrl;
-      }
-    }
-  }
-
-  async scrapeManga(url: string) {
-    const response = await axios.get(url);
-    const html = response.data;
-    const $ = cheerio.load(html);
-
-    const mangaName = $('h1.title-detail').text();
-    const updatedAt = $('#item-detail time.small').text();
-    const status = $('.list-info .status p').text();
-    const author = $('.list-info .author p').text();
-    const views = $('.list-info li:last-child p.col-xs-8').text();
-    const image = $('.detail-info .col-image img').attr('src');
-    const content = $('.detail-content .shortened').text();
-    const totalVotes = $('.col-info .mrt5 span span:last-child').text();
-    const rate = $('.col-info .mrt5 span span:first-child').text();
-    // const categories = $('.list-info .kind p').get();
-    const manga: Manga = {
-      name: mangaName,
-      updatedAt,
-      status,
-      author,
-      views,
-      image: image.slice(2),
-      content,
-      totalVotes,
-      rate
-    }
-
-    return manga;
   }
 }
 
---
+++ src/crawl/index.controller.ts
@@ -0,0 +1,102 @@
+import cheerio from 'cheerio';
+import axios from 'axios';
+import * as fs from 'fs';
+import * as path from 'path';
+
+interface Manga {
+	name: string;
+	updatedAt: string;
+	status?: string;
+	author?: string;
+	categories?: object;
+	views?: string;
+	image?: string;
+	content?: string;
+	totalVotes?: string;
+	rate?: string;
+}
+
+class Crawl {
+  readonly defaultPageUrl = process.env.CRAWL_DOMAIN;
+
+  async scrape(req, res) {
+    const dataRes = [];
+		for await (let url of this.crawlListMangas(this.defaultPageUrl)) {
+      let manga = await this.scrapeManga(url);
+      dataRes.push(manga);
+    }
+
+    return res.json(dataRes);
+	}
+
+  async *crawlListMangas(url: string) {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const links = $('.ModuleContent .items .item').get();
+
+    for (let link of links) {
+      const href = $(link).find('figure.clearfix .image a').attr('href');
+      const mangaPageUrl = new URL(href, url).toString();
+
+      if (mangaPageUrl) {
+        yield mangaPageUrl;
+      }
+    }
+  }
+
+  async scrapeManga(url: string) {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const mangaName = $('h1.title-detail').text();
+    const updatedAt = $('#item-detail time.small').text();
+    const status = $('.list-info .status p').text();
+    const author = $('.list-info .author p').text();
+    const views = $('.list-info li:last-child p.col-xs-8').text();
+    const content = $('.detail-content .shortened').text();
+    const totalVotes = $('.col-info .mrt5 span span:last-child').text();
+    const rate = $('.col-info .mrt5 span span:first-child').text();
+
+    //crawl image and upload this
+    const imageUrl = $('.detail-info .col-image img').attr('src');
+    const image = await this.scrapeImage(imageUrl, url);
+    // const categories = $('.list-info .kind p').get();
+    const manga: Manga = {
+      name: mangaName,
+      updatedAt,
+      status,
+      author,
+      views,
+      image,
+      content,
+      totalVotes,
+      rate
+    }
+
+    return manga;
+  }
+
+  protected async scrapeImage(url: string, baseUrl: string) {
+		const imageUrl = new URL(url, baseUrl).toString();
+		const imagePath = await this.downloadFile(imageUrl, 'image');
+
+		return imagePath;
+	}
+
+	protected async downloadFile(url: string, dir: string) {
+		const response = await axios.get(url, {
+			responseType: 'arraybuffer'
+		});
+
+		fs.mkdirSync(dir, { recursive: true });
+
+		const filePath = path.join(dir, path.basename(url));
+		fs.writeFileSync(filePath, response.data);
+
+		return filePath;
+	}
+}
+
+export default new Crawl()
