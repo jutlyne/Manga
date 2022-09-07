bf4807e517b380d65a4e4e280bc196c24d833113
kyvc-flydino <kyvc@flydinotech.com>
2022-09-07 11:13:22 +0700

    create crawl data list manga

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,25 @@
-import * as express from 'express'
+import 'dotenv/config'
+import * as express from 'express';
+import cheerio from 'cheerio';
+import axios from 'axios';
 
-class App {  
-  public express
+interface Manga {
+	name: string;
+	updatedAt: string;
+	status?: string;
+	author?: string;
+	categories?: object;
+	views?: string;
+	image?: string;
+	content?: string;
+	totalVotes?: string;
+	rate?: string;
+}
+
+class App {
+  public express;
+
+  readonly defaultPageUrl = process.env.CRAWL_DOMAIN;
 
   constructor () {
     this.express = express()
@@ -9,13 +27,62 @@ class App {
   }
 
   private mountRoutes (): void {
-    const router = express.Router()
-    router.get('/', (req, res) => {
-      res.json({
-        message: 'Hello World!'
-      })
+    const router = express.Router();
+    const arr = [];
+    router.get('/', async (req, res) => {
+      for await (let url of this.crawlListMangas(this.defaultPageUrl)) {
+        let a = await this.scrapeManga(url);
+        arr.push(a);
+      }
+      res.json(arr);
     })
     this.express.use('/', router)
+  }
+
+  async *crawlListMangas(url: string) {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+    const links = $('.ModuleContent .items .item').get();
+
+    for (let link of links) {
+      const href = $(link).find('figure.clearfix .image a').attr('href');
+      const mangaPageUrl = new URL(href, url).toString();
+
+      if (mangaPageUrl) {
+        yield mangaPageUrl;
+      }
+    }
+  }
+
+  async scrapeManga(url: string) {
+    const response = await axios.get(url);
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const mangaName = $('h1.title-detail').text();
+    const updatedAt = $('#item-detail time.small').text();
+    const status = $('.list-info .status p').text();
+    const author = $('.list-info .author p').text();
+    const views = $('.list-info li:last-child p.col-xs-8').text();
+    const image = $('.detail-info .col-image img').attr('src');
+    const content = $('.detail-content .shortened').text();
+    const totalVotes = $('.col-info .mrt5 span span:last-child').text();
+    const rate = $('.col-info .mrt5 span span:first-child').text();
+    // const categories = $('.list-info .kind p').get();
+    const manga: Manga = {
+      name: mangaName,
+      updatedAt,
+      status,
+      author,
+      views,
+      image: image.slice(2),
+      content,
+      totalVotes,
+      rate
+    }
+
+    return manga;
   }
 }
 
