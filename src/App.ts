870055fc1aac07aeed2d96e59b3f356d401a01a9
kyvc-flydino <kyvc@flydinotech.com>
2022-09-15 10:14:51 +0700

    change ejs view to hbs

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,9 @@
 import 'dotenv/config'
-import * as express from 'express';
+import { Application } from 'express';
 import apiRoutes from './routes/api';
 import userRoutes from './routes/user';
 
-const routes = (server:express.Application): void => {
+const routes = (server:Application): void => {
   server.use('/api', new apiRoutes().router)
   server.use('/', new userRoutes().router)
 }
